b9f1354fb42a7b4f7fe254490ccbc4d74429a5ef
Mihir Singh <git.service@mihirsingh.com>
2016-04-22 17:15:41 -0400

    Defined a method on the Server type for sending packets

--- sftp/server.go
+++ sftp/server.go
@@ -10,3 +10,18 @@ type Server struct {
 	Out      io.WriteCloser
 	OutMutex *sync.Mutex
 }
+
+func (s *Server) SendPacket(p Packet) (int, error) {
+	encoded, err := p.Marshal()
+
+	if err != nil {
+		return 0, err
+	}
+
+	s.OutMutex.Lock()
+	defer s.OutMutex.Unlock()
+
+	n, err := s.Out.Write(encoded)
+
+	return n, err
+}
