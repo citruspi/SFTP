f7a6a3ee334c342eb2bcf653691f297f36098840
Mihir Singh <git.service@mihirsingh.com>
2016-04-21 21:32:06 -0400

    Implemented unmarshalling of extension pairs

--- sftp/extensions.go
+++ sftp/extensions.go
@@ -6,3 +6,22 @@ type ExtensionPair struct {
 	Name string
 	Data string
 }
+
+func UnmarshalExtensionPair(b []byte) (ExtensionPair, []byte, error) {
+	var extension ExtensionPair
+	var err error
+
+	extension.Name, b, err = UnmarshalStringSafe(b)
+
+	if err != nil {
+		return extension, b, err
+	}
+
+	extension.Data, b, err = UnmarshalStringSafe(b)
+
+	if err != nil {
+		return extension, b, err
+	}
+
+	return extension, b, nil
+}
