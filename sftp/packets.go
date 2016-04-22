1fb6cc84a337369976161cf29e9af0ef8fe59fe1
Mihir Singh <git.service@mihirsingh.com>
2016-04-21 22:06:20 -0400

    Implemented marshalling for the SSHFxInitPacket

--- sftp/packets.go
+++ sftp/packets.go
@@ -36,3 +36,25 @@ type SSHFxInitPacket struct {
 	Version    uint32
 	Extensions []ExtensionPair
 }
+
+func (p SSHFxInitPacket) MarshalBinary() ([]byte, error) {
+	// 1 byte for the packet type and 4 for the version value
+	length := 1 + 4
+
+	for _, extension := range p.Extensions {
+		// 4 bytes per string marshalled + length of each string
+		length += 4 + len(extension.Name) + 4 + len(extension.Data)
+	}
+
+	binary := make([]byte, 0, length)
+
+	binary = append(binary, SSH_FXP_INIT)
+	binary = MarshalUint32(binary, p.Version)
+
+	for _, extension := range p.Extensions {
+		binary = MarshalString(binary, extension.Name)
+		binary = MarshalString(binary, extension.Data)
+	}
+
+	return binary, nil
+}
