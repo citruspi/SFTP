78538ac45f0d9409614a7c3a07f22aaa0079f6a9
Mihir Singh <git.service@mihirsingh.com>
2016-04-22 15:38:22 -0400

    Updated the Marshal test for SSHFxVersionPacket

--- sftp/packets_test.go
+++ sftp/packets_test.go
@@ -42,24 +42,19 @@ func TestSSHFxInitPacketUnmarshal(t *testing.T) {
 	}
 }
 
-func TestSSHFxVersionPacketMarshalBinary(t *testing.T) {
-	var version uint32
-	var extensions []ExtensionPair
-
-	version = uint32(3)
-
+func TestSSHFxVersionPacketMarshal(t *testing.T) {
 	packet := &SSHFxVersionPacket{
-		Version:    version,
-		Extensions: extensions,
+		Version:    uint32(3),
+		Extensions: []ExtensionPair{},
 	}
 
-	marshalled, err := packet.MarshalBinary()
+	marshalled, err := packet.Marshal()
 
 	if err != nil {
 		t.Error(err)
 	}
 
-	expected := []byte{1, 0, 0, 0, 3}
+	expected := []byte{0, 0, 0, 5, 2, 0, 0, 0, 3}
 
 	if !bytes.Equal(marshalled, expected) {
 		t.Errorf("Expected %v, received %v", expected, marshalled)
